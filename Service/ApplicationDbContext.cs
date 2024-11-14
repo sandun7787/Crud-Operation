a254f6276b4e10a4740792ea6fef9767ed0ef390
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-11-14 10:32:59 +0530

    new

---
+++ Service/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Service
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) 
+        
+        {
+            
+        }
+    }
+}
