761b15125ff92eb37578fdee17aa6e220bec4268
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-11-15 10:58:05 +0530

    create a dto and view

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6254675d8b925503de43c7f6ed2993db9b1d8f4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84ceba466e2a5affea9826e96eff63b0a9bf3194")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
