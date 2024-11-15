6254675d8b925503de43c7f6ed2993db9b1d8f4b
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-11-15 09:18:34 +0530

    add-new-service-components

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Service;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var product = context.Products.ToList();
+            return View(product);
+        }
+    }
+}
